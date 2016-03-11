a4e15d66b573ee08e7bab9a585b180ed9b7294df
alanahanson <alanahanson@gmail.com>
2016-03-11 16:30:18 -0600

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+alana = User.create!(username: 'alana', password: 'password', email, email: 'alana@mail.com')
+marshall = User.create!(username: 'marshall', password: 'password', email, email: 'marshall@mail.com')
+mike = User.create!(username: 'mike', password: 'password', email, email: 'mike@mail.com')
+lauren = User.create!(username: 'lauren', password: 'password', email, email: 'lauren@mail.com')
+
+
