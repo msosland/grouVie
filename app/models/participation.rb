1e37662437da24260f2364665664c2d22082cfed
alanahanson <alanahanson@gmail.com>
2016-03-12 10:46:43 -0600

    Update schema with paperclip

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,11 @@
 class Participation < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
+
+  has_attached_file :image, :styles => {:medium => "200x200", :large => "500x500"}
+  validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/}
+
+  def as_json(options={})
+    super(options).merge(image_url: image.url)
+  end
 end
