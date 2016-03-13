7c3d83bf6894fe788b58188fd41cd931e9411fce
Alana Hanson & Michal Klimek <pair+alanahanson.klimekm@devbootcamp.com>
2016-03-13 14:13:54 -0500

    Fix sessions controller for login post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    p "********************************************************"
-    # my_hash = JSON.parse(params)
-    puts params["email"]
-    p "********************************************************"
-
     @user = User.find_by(email: params["email"])
     if @user && @user.authenticate(params['password'])
-      p "USER LOGGED IN!"
-      p @user
-      p @user.to_json
       log_in @user
       render json: @user
-      #on frontend, render user profile page using user.id
     else
       # TODO: something should happen if user login fails
       # render status: 412
