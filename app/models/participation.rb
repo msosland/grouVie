bc6025fcec22c5b03b8568c54c1544e445c4308f
Marshall Sosland <email": null>
2016-03-13 16:03:05 -0500

    alter participation class backend

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,10 +2,12 @@ class Participation < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
 
+  # validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/}
+  do_not_validate_attachment_file_type :image
   has_attached_file :image, :styles => {:medium => "200x200", :large => "500x500"}
-  validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/}
 
   def as_json(options={})
     super(options).merge(image_url: image.url, username: User.find(user_id).username)
   end
 end
+
