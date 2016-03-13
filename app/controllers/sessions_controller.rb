1010004568cb4e294f76ac2c1205307501fb50ef
Alana Hanson & Michal Klimek <pair+alanahanson.klimekm@devbootcamp.com>
2016-03-13 13:06:21 -0500

    Correct login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+   skip_before_action :verify_authenticity_token
   def new
     #render form to enter username and password
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    p "********************************************************"
+    # my_hash = JSON.parse(params)
+    puts params["email"]
+    p "********************************************************"
+
+    @user = User.find_by(email: params["email"])
+    if @user && @user.authenticate(params['password'])
+      p "USER LOGGED IN!"
+      p @user
+      p @user.to_json
+      log_in @user
       render json: @user
       #on frontend, render user profile page using user.id
     else
-      render json: @user.errors, status: 412
+      # TODO: something should happen if user login fails
+      # render status: 412
     end
   end
 
