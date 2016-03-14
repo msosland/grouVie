1ea760ff80a5ac88f09d0edf14239be33daa3edf
Marshall Sosland <email": null>
2016-03-13 19:57:54 -0500

    change update route for particpations

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -19,16 +19,11 @@ class ParticipationsController < ApplicationController
   end
 
   def update
-    # p "***************************"
-    # image = JSON.parse(params.keys[0])["obj"].gsub(/\n/, '').gsub(' ', '+')
-    # p "***************************"
-    # image_data = Paperclip.io_adapters.for(image)
-    # @participation = Participation.find(5)
-    # p @participation.image.url
-    # p "***************************"
-    # @participation.update(image: image_data)
-    # p @participation.image.url
-    # render :json @participation
+    image = JSON.parse(params.keys[0])["obj"].gsub(/\n/, '').gsub(' ', '+')
+    image_data = Paperclip.io_adapters.for(image)
+    @participation = Participation.find(44)
+    @participation.update(image: image_data)
+    render :json @participation
   end
 
   def destroy
