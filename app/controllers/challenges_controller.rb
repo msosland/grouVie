c06af6be2e9f87fa0f0d79c1e7f2a0c66a3aac7b
Michal Klimek <mklimek15@gmail.com>
2016-03-16 17:35:55 -0500

    Remove p statements in the controller

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -10,26 +10,19 @@ class ChallengesController < ApplicationController
   end
 
   def create
-    p params
     challenge_name = JSON.parse(params.keys[0])["challengeName"]
     challenge_description = JSON.parse(params.keys[0])["challengeDescription"]
-    p start_date = Date.rfc3339(JSON.parse(params.keys[0])["startDate"])
-    p end_date = Date.parse(JSON.parse(params.keys[0])["endDate"])
-    p start_date.class
+    start_date = Date.rfc3339(JSON.parse(params.keys[0])["startDate"])
+    end_date = Date.parse(JSON.parse(params.keys[0])["endDate"])
     user_id = JSON.parse(params.keys[0])["userId"].to_i
     @user = User.find_by(id: user_id)
-    p @user
-
     @challenge = Challenge.new(name: challenge_name, description: challenge_description, group_id: params["group_id"].to_i, start_date: start_date, end_date: end_date)
-    #
+    
     @challenge.participants << @user
 
     if @challenge.save
-      p @challenge
       render json: @challenge
     else
-      p @challenge
-      p @challenge.errors
       render json: @challenge.errors, status: 412
     end
   end
